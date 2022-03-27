0e0bf207b77963e1635ee242326fe3e624a08393
Shad0w7 <ayush.nayak@gmail.com>
2022-03-27 12:43:53 -0700

    Initial Commit

---
+++ iTunesDiscordRPC/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iTunesDiscordRPC
+//
+//  Created by Ayush Nayak on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
